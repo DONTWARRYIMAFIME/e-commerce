e33af994ec2ee84b3a5b7b3c9c932dc84e65e6a2
ulas-kastsiukovich <ulas.kastsiukovich@gmail.com>
2023-03-27 18:40:11 +0300

    Add user seeder

--- src/seeder.sql
+++ src/seeder.sql
@@ -1,6 +1,7 @@
 DELETE FROM "email_address" CASCADE;
-DELETE FROM "role" CASCADE;
 DELETE FROM "user" CASCADE;
+DELETE FROM "role" CASCADE;
+DELETE FROM "user_roles" CASCADE;
 DELETE FROM "language" CASCADE;
 
 INSERT INTO "role" (name)
@@ -10,16 +11,55 @@ VALUES
     ('customer_support'),
     ('admin');
 
--- WITH new_email_address AS (
---     INSERT INTO "email_address" (address, name)
---     VALUES
---         ('user@gmail.com', 'Test User')
---     RETURNING id
--- )
--- INSERT INTO "user" (email_address_id, password)
--- SELECT e.id, '$argon2id$v=19$m=65536,t=3,p=4$513t2PfDXwcVaWJy1ycC$gJhSyuk+EzHbQ3aoSv4KfTad0o1VrsCB+jg9tVeyyH0' FROM new_email_address e;
-
 INSERT INTO "language" (code, name)
 VALUES
     ('en', 'English'),
     ('ru', 'Russian');
+
+-- Customer user
+WITH new_email_address AS (
+    INSERT INTO "email_address" (address, name)
+    VALUES
+        ('customer@gmail.com', 'User Customer')
+    RETURNING id
+)
+INSERT INTO "user" (email_address_entity_id, first_name, last_name, password)
+SELECT e.id, 'User', 'Customer', '$argon2id$v=19$m=65536,t=3,p=4$513t2PfDXwcVaWJy1ycC$gJhSyuk+EzHbQ3aoSv4KfTad0o1VrsCB+jg9tVeyyH0' FROM new_email_address e;
+INSERT INTO "user_roles" (user_id, role_id)
+SELECT u.id, r.id FROM "user" u INNER JOIN email_address ea on ea.id = u.email_address_entity_id CROSS JOIN "role" r WHERE ea.address = 'customer@gmail.com' AND r.name = 'customer';
+
+-- Partner user
+WITH new_email_address AS (
+    INSERT INTO "email_address" (address, name)
+        VALUES
+            ('partner@gmail.com', 'User Partner')
+        RETURNING id
+)
+INSERT INTO "user" (email_address_entity_id, first_name, last_name, password)
+SELECT e.id, 'User', 'Partner', '$argon2id$v=19$m=65536,t=3,p=4$513t2PfDXwcVaWJy1ycC$gJhSyuk+EzHbQ3aoSv4KfTad0o1VrsCB+jg9tVeyyH0' FROM new_email_address e;
+INSERT INTO "user_roles" (user_id, role_id)
+SELECT u.id, r.id FROM "user" u INNER JOIN email_address ea on ea.id = u.email_address_entity_id CROSS JOIN "role" r WHERE ea.address = 'partner@gmail.com' AND r.name = 'partner';
+
+-- Customer support user
+WITH new_email_address AS (
+    INSERT INTO "email_address" (address, name)
+        VALUES
+            ('customer_support@gmail.com', 'User Customer Support')
+        RETURNING id
+)
+INSERT INTO "user" (email_address_entity_id, first_name, last_name, password)
+SELECT e.id, 'User', 'Customer Support', '$argon2id$v=19$m=65536,t=3,p=4$513t2PfDXwcVaWJy1ycC$gJhSyuk+EzHbQ3aoSv4KfTad0o1VrsCB+jg9tVeyyH0' FROM new_email_address e;
+INSERT INTO "user_roles" (user_id, role_id)
+SELECT u.id, r.id FROM "user" u INNER JOIN email_address ea on ea.id = u.email_address_entity_id CROSS JOIN "role" r WHERE ea.address = 'customer_support@gmail.com' AND r.name = 'customer_support';
+
+-- Admin user
+WITH new_email_address AS (
+    INSERT INTO "email_address" (address, name)
+        VALUES
+            ('admin@gmail.com', 'User Admin')
+        RETURNING id
+)
+INSERT INTO "user" (email_address_entity_id, first_name, last_name, password)
+SELECT e.id, 'User', 'Admin', '$argon2id$v=19$m=65536,t=3,p=4$513t2PfDXwcVaWJy1ycC$gJhSyuk+EzHbQ3aoSv4KfTad0o1VrsCB+jg9tVeyyH0' FROM new_email_address e;
+INSERT INTO "user_roles" (user_id, role_id)
+SELECT u.id, r.id FROM "user" u INNER JOIN email_address ea on ea.id = u.email_address_entity_id CROSS JOIN "role" r WHERE ea.address = 'admin@gmail.com' AND r.name = 'admin';
